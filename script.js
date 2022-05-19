b82a854c5f041925799999de1956318699f7b842
svoncoelln <89931312+svoncoelln@users.noreply.github.com>
2022-05-19 09:57:57 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+recieve();
+
 function recieve() {
     let xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             response = JSON.parse(this.responseText);
-            console.log(this.response)
-        }
+            display(response);
+    }
     };
-    xmlhttp.open("GET", "server.php?q=" + response, true);
+    xmlhttp.open("GET", "server.php", true);
     xmlhttp.send();
 }
 
-console.log(recieve());
+function display(data) {
+    let output = "";
+    for (let i = 0; i < data.length; i++) {
+        output += "<div class='col-md-4'><div class='card'><div class='card-body'><h5 class='card-title'>" + data[i].title + "</h5><p class='card-text'>" + data[i].description + "</p><a href='#' class='btn btn-primary'>Read more</a></div></div></div>";
+    }
+    document.getElementById("output").innerHTML = output;
+}
